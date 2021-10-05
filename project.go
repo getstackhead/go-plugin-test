1286702ed1d074ceeff0c4a646b01ac0643c1aab
Mario Lubenka <mario.lubenka@googlemail.com>
2021-10-05 19:28:39 +0200

    feat: add stubs

---
+++ project.go
@@ -0,0 +1,11 @@
+package pluginlib
+
+type Project struct {
+	Name string
+}
+
+func GetProject() Project {
+	// mock function
+	// behaviour is implemented in StackHead main repository
+	return Project{}
+}
--- stackhead.go
+++ stackhead.go
@@ -1,6 +1,11 @@
 package pluginlib
 
-func StackHeadExecute(command string) {
+func StackHeadExecute(command string, args interface{}) {
+	// mock function
+	// behaviour is implemented in StackHead main repository
+}
+
+func InstallPackage(packages interface{}) {
 	// mock function
 	// behaviour is implemented in StackHead main repository
 }
